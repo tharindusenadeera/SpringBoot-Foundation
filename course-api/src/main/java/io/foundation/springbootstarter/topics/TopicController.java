add3c1027d151ced7920f5e4ab54643cc602db9f
Tharindu <tharindusenadeera081@gmail.com>
2017-12-15 23:41:15 +0530

    Initial Commit

--- course-api/src/main/java/io/foundation/springbootstarter/topics/TopicController.java
+++ course-api/src/main/java/io/foundation/springbootstarter/topics/TopicController.java
@@ -1,21 +1,27 @@
 package io.foundation.springbootstarter.topics;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TopicController {
 	
+	@Autowired
+	private TopicService topicService;
+	
 	@RequestMapping("/topics")
 	public List<Topic> getAllTopics() {
 		
-		return Arrays.asList(
-				new Topic("Spring", "Spring Frame work", "Spring Framework Desc"),
-				new Topic("Spring", "Spring Frame work", "Spring Framework Desc")
-				);
+		return topicService.getAllTopics();
+	}
+	
+	@RequestMapping("/topics/{id}")
+	public Topic getTopic(@PathVariable String id) {
+		return topicService.getTopic(id);
 	}
 
 }
--- course-api/src/main/java/io/foundation/springbootstarter/topics/TopicService.java
+++ course-api/src/main/java/io/foundation/springbootstarter/topics/TopicService.java
@@ -1,5 +1,26 @@
 package io.foundation.springbootstarter.topics;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
 public class TopicService {
+	
+	private List<Topic> topics = Arrays.asList(
+			new Topic("Spring", "Spring Frame work", "Spring Framework Desc"),
+			new Topic("Java", "Spring Frame work", "Spring Framework Desc")
+			);
+	
+	public List<Topic> getAllTopics(){
+		return topics;
+		
+	}
+	
+	
+	public Topic getTopic(String id) {
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
 
 }
